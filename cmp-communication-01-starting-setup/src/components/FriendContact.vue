3166fb85748ef5a8ac96d42348c908255c3add93
ggarber42 <ggarber@protonmail.com>
2022-01-10 00:55:25 -0300

    lesson 8

---
+++ cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -0,0 +1,58 @@
+<template>
+  <li>
+    <h2>{{ name}} {{ isFavourite === true ? '(Favourite)' : '' }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <button @click="toggleFavourite">toogleFavourite</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    id:{
+      type: String,
+      required: true
+    },
+    name:{
+      type: String,
+      required: true
+    },
+    phoneNumber:{
+      type: String,
+      required: true
+    },
+    emailAddress:{
+      type: String,
+      required: true
+    },
+    isFavourite: {
+      type: Boolean,
+      required: true,
+      default: false,
+    }
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavourite(){
+      this.$emit('toggle-favourite',this.id);
+    }
+  }
+};
+</script>
